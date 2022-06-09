a44ece6b55ee07c567bc9dd69d913d985e443cd5
OliaVladova <ovladova1@gmail.com>
2022-06-09 21:22:32 +0300

    Add files via upload

---
+++ CounterStrike/models/players/PlayerImpl.java
@@ -0,0 +1,101 @@
+package CounterStriker.src.CounterStriker.models.players;
+
+import CounterStriker.src.CounterStriker.common.ExceptionMessages;
+import CounterStriker.src.CounterStriker.models.guns.Gun;
+
+public abstract class PlayerImpl implements Player {
+    private String username;
+    private int health;
+    private int armor;
+    private boolean isAlive;
+    private Gun gun;
+
+    protected PlayerImpl(String username, int health, int armor, Gun gun) {
+        this.setUsername(username);
+        this.setHealth(health);
+        this.setArmor(armor);
+        this.setGun(gun);
+    }
+
+    private void setUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.INVALID_PLAYER_NAME);
+        }
+        this.username = username;
+    }
+
+    private void setHealth(int health) {
+        if (health < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PLAYER_HEALTH);
+        }
+        this.health = health;
+    }
+
+    private void setArmor(int armor) {
+        if (armor < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PLAYER_ARMOR);
+        }
+        this.armor = armor;
+    }
+
+    private void setGun(Gun gun) {
+        if (gun == null) {
+            throw new NullPointerException(ExceptionMessages.INVALID_GUN);
+        }
+        this.gun = gun;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public int getArmor() {
+        return this.armor;
+    }
+
+    @Override
+    public Gun getGun() {
+        return this.gun;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    @Override
+    public void takeDamage(int points) {
+        int damage = points;
+
+        if (damage >= this.getArmor()) {
+            damage -= this.getArmor();
+            this.armor = 0;
+            this.health -= damage;
+        } else {
+            this.armor -= damage;
+        }
+
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder info = new StringBuilder();
+
+        info.append(String.format("%s: %s", this.getClass().getSimpleName(), this.username)).append(System.lineSeparator());
+        info.append(String.format("--Health: %d", this.health)).append(System.lineSeparator());
+        info.append(String.format("--Armor: %d", this.armor)).append(System.lineSeparator());
+        info.append(String.format("--Gun: %s", this.gun.getName()));
+
+        return info.toString().trim();
+    }
+}
