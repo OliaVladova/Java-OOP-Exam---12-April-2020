e987a23003a54a0670a8ddc2fd9c14ee4d4455fd
OliaVladova <ovladova1@gmail.com>
2022-06-09 21:21:22 +0300

    Add files via upload

---
+++ CounterStrike/models/guns/Rifle.java
@@ -0,0 +1,19 @@
+package CounterStriker.src.CounterStriker.models.guns;
+
+
+public class Rifle extends GunImpl{
+    private static final int BULLETS_TO_FIRE = 10;
+
+    public Rifle(String name, int bulletsCount) {
+        super(name, bulletsCount);
+    }
+
+    @Override
+    public int fire() {
+        if (super.getBulletsCount() < BULLETS_TO_FIRE) {
+            return 0;
+        }
+        super.decreaseBullets(BULLETS_TO_FIRE);
+        return BULLETS_TO_FIRE;
+    }
+}
